9fc675cd089282f236338aeadab31c0ff3530506
mina998 <iosss@qq.com>
2022-05-28 09:43:15 +0800

    lsws

--- scripts/ovps.sh
+++ scripts/ovps.sh
@@ -2,6 +2,8 @@
 
 bbrNetfilter(){
 
+	apt update
+
 	if [ ! `command -v vim` ] ; then
 		apt purge vim-common -y
 		apt install vim -y
@@ -137,19 +139,74 @@ certSSL(){
 	echo "证书全链: $sslSaveRoot/fullchain.pem"
 }
 
+# 安装MariaDB数据库
+installMariaDB(){
+
+	# 安装依赖
+	apt-get install software-properties-common dirmngr
+	# 添加密钥
+	apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+	# 选择系统
+	echo "(1)ubuntu 18.04 (2)ubuntu 20.04 (3)debian 9 (4)debian 10 (5)debian 11"
+    read -p "请选择:" num
+    if [ $num -eq 1 ]; then
+        # ubuntu 18.04
+		add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.gigenet.com/mariadb/repo/10.5/ubuntu bionic main'
+    elif [ $num -eq 2 ] ; then
+    	# ubuntu 20.04
+		add-apt-repository 'deb [arch=amd64,arm64,ppc64el,s390x] https://mirrors.gigenet.com/mariadb/repo/10.5/ubuntu focal main'
+	elif [ $num -eq 3 ]; then
+		# debian 9
+		add-apt-repository 'deb [arch=amd64,i386,ppc64el,arm64] https://mirrors.gigenet.com/mariadb/repo/10.5/debian stretch main'
+	elif [ $num -eq 4 ]; then
+		# debian 10
+		add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.gigenet.com/mariadb/repo/10.5/debian buster main'
+    elif [ $num -eq 5 ]; then
+        # debian 11
+		add-apt-repository 'deb [arch=amd64,i386,arm64,ppc64el] https://mirrors.gigenet.com/mariadb/repo/10.5/debian bullseye main'
+	else
+		echo "无效输入"
+		exit 0
+    fi
+
+    apt update
+	apt install mariadb-server
+	# 重启防止出错
+	systemctl restart mariadb
+
+	echo "向导说明"
+	echo "Enter current password for root (enter for none):   #提示你输入root密码, 没有密码, 直接回车"
+	echo "Switch to unix_socket authentication [Y/n] n        #是否切换到unix套接字身份验证"
+	echo "Change the root password? [Y/n] Y                   #是否为 root 用户设置密码"
+	echo "Remove anonymous users? [Y/n] Y                     #删除匿名用户"
+	echo "Disallow root login remotely? [Y/n] Y               #禁止root用户远程登陆"
+	echo "Remove test database and access to it? [Y/n] Y      #删除测试数据库"
+	echo "Reload privilege tables now? [Y/n] Y                #重新加载权限表"
+
+	# 运行配置向导
+	mysql_secure_installation
+
+}
+
+
 
 menu(){
     echo "(1)系统设置(防火墙,编辑器,BBR)"
     echo "(2)安装OpenLiteSpeed"
-    echo "(3)申请SSL证书"
+    echo "(3)安装MariaDB"
+    echo "(4)申请SSL证书"
     read -p "请选择:" num
     if [ $num -eq 1 ]; then
         bbrNetfilter
     elif [ $num -eq 2 ] ; then
     	installOls
+    elif [ $num -eq 3 ] ; then
+		installMariaDB
+    elif [ $num -eq 4 ] ; then
+		certSSL
     else
-        certSSL
+        echo "输入无效"
+        exit 0
     fi
 }
 menu
-
